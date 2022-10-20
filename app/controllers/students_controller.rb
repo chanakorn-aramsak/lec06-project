84785ef7dd4a769630c27f20c3babffcb13f57ac
chanakorn aramsak <solo20113@gmail.com>
2022-10-20 13:54:38 +0700

    add comment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: %i[ show edit update destroy ]
+  before_action :set_student, only: %i[ show edit update destroy ] # ทุก function ใน code จะต้องเรียกใช้งาน function ชื่อ set_student ก่อน
 
   # GET /students or /students.json
   def index
